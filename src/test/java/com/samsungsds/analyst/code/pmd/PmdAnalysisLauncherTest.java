61d8fc89b263bbbf0fbb1297f6e366d7d8af80a0
Walter Cho <szro.cho@samsung.com>
2017-12-29 12:37:13 +0900

    modify assertEquals to assertTrue

--- src/test/java/com/samsungsds/analyst/code/pmd/PmdAnalysisLauncherTest.java
+++ src/test/java/com/samsungsds/analyst/code/pmd/PmdAnalysisLauncherTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.isA;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
@@ -107,7 +108,7 @@ public class PmdAnalysisLauncherTest {
 		
 		pmdAnalysisLauncher.run(testInstanceKey);
 		
-		assertEquals(true, arg.contains(path));
+		assertTrue(arg.contains(path));
 	}
 	
 	@Test public void 
