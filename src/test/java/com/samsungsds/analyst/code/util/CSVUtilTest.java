78df31c72aab66586d14efea32489f2a020d5f4d
Vincent Han <switchover@gmail.com>
2020-09-04 18:46:32 +0900

    Test 수정

--- src/test/java/com/samsungsds/analyst/code/util/CSVUtilTest.java
+++ src/test/java/com/samsungsds/analyst/code/util/CSVUtilTest.java
@@ -1,70 +1,68 @@
 package com.samsungsds.analyst.code.util;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
 
-import com.samsungsds.analyst.code.util.CSVUtil;
-
 public class CSVUtilTest {
-	
+
 	@Test
 	public void testWithNormalString() {
 		// arrange
 		String normalString = "aaa";
-		
+
 		// act
 		String result = CSVUtil.getCSVStyleString(normalString);
-		
+
 		// assert
 		assertThat(result, is("aaa"));
 	}
-	
+
 	@Test
 	public void testWithSingleQuoteAndCommaString() {
 		// arrange
 		String normalString = "'a', 'b'";
-		
+
 		// act
 		String result = CSVUtil.getCSVStyleString(normalString);
-		
+
 		// assert
 		assertThat(result, is("\"'a', 'b'\""));
 	}
-	
+
 	@Test
 	public void testWithDoubleQuote() {
 		// arrange
 		String normalString = "\"abc\"";
-		
+
 		// act
 		String result = CSVUtil.getCSVStyleString(normalString);
-		
+
 		// assert
 		assertThat(result, is("\"\"\"abc\"\"\""));
 	}
-	
+
 	@Test
 	public void testWithCommaString() {
 		// arrange
 		String normalString = "a, b";
-		
+
 		// act
 		String result = CSVUtil.getCSVStyleString(normalString);
-		
+
 		// assert
 		assertThat(result, is("\"a, b\""));
 	}
-	
+
 	@Test
 	public void testWithNewLineString() {
 		// arrange
 		String normalString = "aaa\nbbb";
-		
+
 		// act
 		String result = CSVUtil.getCSVStyleString(normalString);
-		
+
 		// assert
 		assertThat(result, is("\"aaa\nbbb\""));
 	}
