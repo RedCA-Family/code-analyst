c700494b48020be8c89e5ce75c07ee4e093b5af3
Vincent Han <switchover@gmail.com>
2021-01-25 12:01:36 +0900

    ESLint multi src 처리

---
+++ src/main/webapp/script/test.js
@@ -0,0 +1,94 @@
+var merged = arr.reduce(function(a, b) {
+    a.concat(b);
+}); // Noncompliant: No return statement
+
+for (;;) { // Noncompliant; end condition omitted
+}
+
+function test() {
+    var duration = + new Date() - start.time,
+        isPastHalf = Number(duration) < 250 && Math.abs(delta.x) > 20 || Math.abs(delta.x) > viewport / 2,
+        direction = delta.x < 0;
+
+    if (!isScrolling) {
+        if (isPastHalf) {
+            if (direction) {
+                this.close();
+            } else {
+                if (this.content.getBoundingClientRect().left > viewport / 2 && pulled === true) {
+                    this.close();
+                    return;
+                }
+                this.open();
+            }
+        } else {
+            if (this.content.getBoundingClientRect().left > viewport / 2) {
+                if (this.isEmpty(delta) || delta.x > 0) {
+                    this.close();
+                    return;
+                }
+                this.open();
+                return;
+            }
+            this.close();
+        }
+    }
+}
+
+function test2() {
+    var duration = + new Date() - start.time,
+        isPastHalf = Number(duration) < 250 && Math.abs(delta.x) > 20 || Math.abs(delta.x) > viewport / 2,
+        direction = delta.x < 0;
+
+    if (!isScrolling) {
+        if (isPastHalf) {
+            if (direction) {
+                this.close();
+            } else {
+                if (this.content.getBoundingClientRect().left > viewport / 2 && pulled === true) {
+                    this.close();
+                    return;
+                }
+                this.open();
+            }
+        } else {
+            if (this.content.getBoundingClientRect().left > viewport / 2) {
+                if (this.isEmpty(delta) || delta.x > 0) {
+                    this.close();
+                    return;
+                }
+                this.open();
+                return;
+            }
+            this.close();
+        }
+    }
+
+    var duration = + new Date() - start.time,
+        isPastHalf = Number(duration) < 250 && Math.abs(delta.x) > 20 || Math.abs(delta.x) > viewport / 2,
+        direction = delta.x < 0;
+
+    if (!isScrolling) {
+        if (isPastHalf) {
+            if (direction) {
+                this.close();
+            } else {
+                if (this.content.getBoundingClientRect().left > viewport / 2 && pulled === true) {
+                    this.close();
+                    return;
+                }
+                this.open();
+            }
+        } else {
+            if (this.content.getBoundingClientRect().left > viewport / 2) {
+                if (this.isEmpty(delta) || delta.x > 0) {
+                    this.close();
+                    return;
+                }
+                this.open();
+                return;
+            }
+            this.close();
+        }
+    }
+}
